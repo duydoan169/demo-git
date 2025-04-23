cbfacd3788c1c50980ef7b4021eb63cff261e61b
duydoan169 <hanh250470@gmail.com>
2025-04-23 12:31:42 +0700

    adminExamManager

---
+++ js/adminExamManager.js
@@ -0,0 +1,8 @@
+function addExamPopUp(){
+    document.getElementsByClassName("modal-overlay")[0].style.display="block";
+    document.getElementsByClassName("modal")[0].style.display="block";
+}
+function closeAddStudentPopUp(){
+    document.getElementsByClassName("modal-overlay")[0].style.display="none";
+    document.getElementsByClassName("modal")[0].style.display="none";
+}
